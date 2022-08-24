647a5ad9dc5285819b5246b434d46e65db5a957d
Cesar Roman <cromansantillan@deloitte.com>
2022-08-24 14:08:25 -0500

    adding ProjectId for Cypress cloud dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "5gc7by",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
